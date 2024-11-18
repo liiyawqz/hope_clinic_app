9aa10dbd665ff21041401077919500414a01f9b2
”Aliia <bolotova_a@iuca.kg>
2024-11-18 12:09:46 +0600

    feat: create button for sorting

--- src/views/EquipmentView.vue
+++ src/views/EquipmentView.vue
@@ -1,16 +1,20 @@
 <template>
   <v-container>
     <AnalysisBoxes @addEquipment="addEquipment" />
+
     <EquipmentTable :equipments="equipmentData" />
   </v-container>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import {ref} from 'vue'
 import EquipmentTable from '@/components/EquipmentTable.vue'
 import AnalysisBoxes from '@/components/AnalysisBoxes.vue'
+
+
 const equipmentData = ref([])
 const addEquipment = (newEquipment) => {
   equipmentData.value.push(newEquipment)
 }
 </script>
+
